ce606f9c17bd8f8f524331c4b528b682439d35fb
Черепахин Василий <vasi.che@gmail.com>
2023-09-25 16:12:07 +0500

    Home Типовое использование с extend и export

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
-import Home from './components/Home';
+import {Home} from './components/Home';
 
 import RouteScreen1 from './components/RouteScreen1'; // RouteScreen1.js это скрин с TextContainer 
 
 export default function App() {
   return (
     <Layout>
+      {/* Связь экранов с url
+        Конфигурирование Route.
+        КАК ОФОРМИТЬ сам ПУНКТ меню описано в navItems.js (cвязь через "path")
+        path также будет с строке браузера.
+      . element - СТРАНИЦА перехода RouteScreen1 будет отображена при выборе в /route-from-navitems
+      */}
       <Routes>
         <Route path="/" element={<Home />} />
-        {/*
-          Конфигурирование Route.
-          КАК ОФОРМИТЬ сам ПУНКТ меню описано в navItems.js (cвязь через "path")
-          path также будет с строке браузера.
-        . element - СТРАНИЦА перехода RouteScreen1 будет отображена при выборе в /route-from-navitems
-        */}
         {/* RouteScreen1.js это TextContainer */}
         <Route path="/route-from-navitems" element={<RouteScreen1 />} />
       </Routes>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import { TextContainer, Typography } from 'react-md';
+import PropTypes, { number, string, shape } from 'prop-types';
 
-export default function Home() {
-  return (
-    <TextContainer>
-      <Typography type="headline-4">Hello, world!</Typography>
-    </TextContainer>
-  );
+// Работает
+// export default function Home() {
+//   return (
+//     <TextContainer>
+//       <Typography type="headline-4">Home</Typography>
+//     </TextContainer>
+//   );
+// }
+
+// Работает. Типовое использование с 'extend' и 'export'.
+class Home extends React.Component {
+  render() {
+    return (
+      <TextContainer>
+        <Typography type="headline-4">Home Типовое использование с 'extend' и 'export'</Typography>
+      </TextContainer>
+    );
+  }
 }
+
+export { Home };
