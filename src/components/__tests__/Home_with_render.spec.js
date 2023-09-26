8f01498eabd9ed82a24f6cf3669070b1549f5de8
Черепахин Василий <vasi.che@gmail.com>
2023-09-26 15:17:36 +0500

    Home.spec.js - add tests show wrapper.debug(), const {asFragment, getByText} = render(<Home />)

---
+++ src/components/__tests__/Home_with_render.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { shallow, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Home } from '../Home';
+
+// Раскидать по разным файлам тесты с разной методикой
+configure({ adapter: new Adapter() });
+
+describe('Home with render', () => {
+    it('const {asFragment, getByText} = render(<Home />)', () => {
+      // from https://flexiple.com/react/react-testing-library-cheat-sheet
+      const {asFragment, getByText} = render(<Home />);
+      expect(getByText('aaaa')).toBeInTheDocument()
+  });
+})
+
