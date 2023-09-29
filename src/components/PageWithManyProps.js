7c29d2b779e9fa19c6f6c4faa0ab405c1eeb14d0
Черепахин Василий <vasi.che@gmail.com>
2023-09-29 12:20:28 +0500

    demo use many props

--- src/components/PageWithManyProps.js
+++ src/components/PageWithManyProps.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { TextContainer, Typography } from 'react-md';
+import { useParams } from 'react-router-dom';
 
 // for import use 'import PageWithManyElements from './components/PageWithManyElements'
 // const PageWithProps = ( {msg} ) => {
 function PageWithManyProps(props) {
+  let fromUseParams = useParams()
+  console.log(props) // -> {prop1: 'val_prop1', prop2: 'val_prop2'}
   return (
     <div>
       <TextContainer>
@@ -15,6 +18,7 @@ function PageWithManyProps(props) {
         */}
         <Typography type="body-1">val prop1='{props.prop1}'</Typography>
         <Typography type="body-1">val prop2='{props.prop2}'</Typography>
+        {/* <Typography type="body-1">val fromUseParams='{fromUseParams}'</Typography> */}
       </TextContainer>
     </div>
   );
