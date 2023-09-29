703377f0a9d9c948bc0377281a04777c7458ccb6
Черепахин Василий <vasi.che@gmail.com>
2023-09-29 12:06:48 +0500

    demo use many props

---
+++ src/components/PageWithManyProps.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { TextContainer, Typography } from 'react-md';
+
+// for import use 'import PageWithManyElements from './components/PageWithManyElements'
+// const PageWithProps = ( {msg} ) => {
+function PageWithManyProps(props) {
+  return (
+    <div>
+      <TextContainer>
+        <Typography type="body-1">PageWithManyProps</Typography>
+        {/* 
+          Использовано в App.js, как:
+          <Route path="/page-with-many-props" element={<PageWithManyProps prop1="val_prop1" prop2="val_prop2"/>} />
+          внимание к определению prop1, prop2
+        */}
+        <Typography type="body-1">val prop1='{props.prop1}'</Typography>
+        <Typography type="body-1">val prop2='{props.prop2}'</Typography>
+      </TextContainer>
+    </div>
+  );
+}
+
+export { PageWithManyProps };    
