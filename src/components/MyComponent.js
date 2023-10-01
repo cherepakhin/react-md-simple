795d331e43da7d4e4ca40c2d50bd352571647d2a
Черепахин Василий <vasi.che@gmail.com>
2023-10-01 13:54:39 +0500

    bold msg1

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 Пример с https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/props.html
  */
 function MyComponent(props) {
-    const { msgMyComp, msg2 } = props;
+    const { msg1, msg2 } = props;
     return (
         <div>
-        <div >This is MyComponent with props.msgMyComp to msg={msgMyComp}</div>
+        <div >This is MyComponent with props.msg1=<b>{msg1}</b></div>
         <div >and props.msg2={msg2}</div>
         </div>
     );
 }
 
 MyComponent.propTypes = {
-    msgMyComp: PropTypes.string.isRequired,
+    msg1: PropTypes.string.isRequired,
     msg2: PropTypes.string.isRequired,
 };
 
