dd216227280fc8036bf37f9f2317cde573341224
Черепахин Василий <vasi.che@gmail.com>
2023-10-02 00:44:26 +0500

    tests PageWithMsg_with_render.spec.js

--- src/components/__tests__/PageWithMsg_with_render.spec.js
+++ src/components/__tests__/PageWithMsg_with_render.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount, configure} from 'enzyme';
+import { shallow, mount, configure } from 'enzyme';
 import { render } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import { PageWithMsg } from '../PageWithMsg';
@@ -8,25 +8,42 @@ import { PageWithMsg } from '../PageWithMsg';
 configure({ adapter: new Adapter() });
 
 describe('PageWithMsg with mount', () => {
-    it('render(<PageWithMsg props={props_msg}/>)', () => {
-      const props = {
-        msg: "MESSAGE1"
-      }
-      //const wrapper = mount(<PageWithProps props={msg}/>);  // TypeError: Cannot read properties of undefined (reading 'child')
-      // const  wrapper = shallow(<PageWithMsg {...props}/>);
-      
-      const  wrapper = shallow(<PageWithMsg msg="MESSAGE1"/>);
-      // '#' означает поиск по id <div id="msgid">...</>
-      expect(wrapper.find('#msgid').html()).toContain('MESSAGE1');
-      // expect(wrapper.props().includedProp).to.equal('Success!');
-      // expect(wrapper.props().msg).to.equal('MESSAGE1');
-      // const message = wrapper.find('.msgid');
-      // console.log("++++MESSAGE");
-      // console.log(message);
-      // const message=result.container.querySelector('#msgid');
-      //expect(message).toEqual("MESSAGE1");
-      // console.log(wrapper.props().children);
-      // expect(wrapper.props().children.includes("MESSAGE1")).toEqual(true);
+  it('render(<PageWithMsg msg="MESSAGE1"/>)', () => {
+    const props = {
+      msg: "MESSAGE1"
+    }
+    //const wrapper = mount(<PageWithProps props={msg}/>);  // TypeError: Cannot read properties of undefined (reading 'child')
+    // const  wrapper = shallow(<PageWithMsg {...props}/>);
+
+    const wrapper = shallow(<PageWithMsg msg="MESSAGE1" />);
+    // '#' означает поиск по id <div id="msgid">...</>
+    expect(wrapper.find('#msgid').html()).toContain('MESSAGE1');
   });
+  it('render(<PageWithMsg with props/>)', () => {
+    const props = {
+      msg: "MESSAGE1"
+    }
+    const wrapper = shallow(<PageWithMsg {...props} />);
+    // '#' означает поиск по id <div id="msgid">...</>
+    expect(wrapper.find('#msgid').html()).toContain('MESSAGE1');
+  });
+  it('render(<PageWithMsg with props/>) test equal', () => {
+    const props = {
+      msg: "MESSAGE1"
+    }
+    const wrapper = shallow(<PageWithMsg {...props} />);
+    // '#' означает поиск по id <div id="msgid">...</>
+    expect(wrapper.find('#msgid').html()).toContain('MESSAGE1');
+  });
+
+  // expect(wrapper.props().includedProp).to.equal('Success!');
+  // expect(wrapper.props().msg).to.equal('MESSAGE1');
+  // const message = wrapper.find('.msgid');
+  // console.log("++++MESSAGE");
+  // console.log(message);
+  // const message=result.container.querySelector('#msgid');
+  //expect(message).toEqual("MESSAGE1");
+  // console.log(wrapper.props().children);
+  // expect(wrapper.props().children.includes("MESSAGE1")).toEqual(true);
 })
 
